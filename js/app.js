3aa3ac9019babc41a1899e5432365c01740f3183
cgxgb123 <christian.blunt12@gmail.com>
2025-11-09 11:13:28 -0500

    added displayList function, added localStorage to load strings on browser refresh

---
+++ js/app.js
@@ -0,0 +1,48 @@
+const taskName = document.getElementById("taskName");
+const catergory = document.getElementById("category");
+const deadline = document.getElementById("deadline");
+const status = document.getElementById("status");
+const addTask = document.getElementById("addTask");
+const taskList = document.getElementById("taskList");
+
+// save and load tasks on refresh
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+function displayTasks(filteredTasks = tasks) {
+  taskList.innerHTML = "";
+
+  filteredTasks.forEach((task, index) => {
+    const row = document.createElement("tr");
+
+    row.innerHTML = `
+      <td>${task.name}</td>
+      <td>${task.category}</td>
+      <td>${task.deadline}</td>
+      <td>
+        <select data-index="${index}" class="status-dropdown">
+          <option value="In Progress" ${
+            task.status === "In Progress" ? "selected" : ""
+          }>In Progress</option>
+          <option value="Completed" ${
+            task.status === "Completed" ? "selected" : ""
+          }>Completed</option>
+          <option value="Overdue" ${
+            task.status === "Overdue" ? "selected" : ""
+          }>Overdue</option>
+        </select>
+      </td>
+    `;
+
+    taskList.appendChild(row);
+  });
+
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+/* Testing tasks, browser storage, and pushing to array
+tasks.push({
+  name: "nameTest",
+  category: "categoryTest",
+  status: "statusTest",
+  deadline: "deadlineTest",
+});
+console.log(displayTasks()); */
